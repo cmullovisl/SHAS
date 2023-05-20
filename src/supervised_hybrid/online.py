55d0509dd4504641f5dec4865dbc53b20f3b3caf
Carlos Mullov <carlos.mullov@kit.edu>
2023-05-20 17:57:27 +0200

    clean up online segmenter

--- src/supervised_hybrid/online.py
+++ src/supervised_hybrid/online.py
@@ -1,123 +1,20 @@
 from typing import Tuple
 import argparse
-from dataclasses import dataclass
-from multiprocessing import cpu_count
 from pathlib import Path
 
 import numpy as np
 import torch
-import yaml
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 from constants import HIDDEN_SIZE, TARGET_SAMPLE_RATE
 from data import FixedSegmentationDatasetNoTarget, segm_collate_fn
-from eval import infer
 from models import SegmentationFrameClassifer, prepare_wav2vec
-
-
-@dataclass
-class Segment:
-    start: float
-    end: float
-    probs: np.array
-    decimal: int = 4
-
-    @property
-    def duration(self):
-        return float(round((self.end - self.start) / TARGET_SAMPLE_RATE, self.decimal))
-
-    @property
-    def offset(self):
-        return float(round(self.start / TARGET_SAMPLE_RATE, self.decimal))
-
-    @property
-    def offset_plus_duration(self):
-        return round(self.offset + self.duration, self.decimal)
-
-
-def trim(sgm: Segment, threshold: float) -> Segment:
-    """reduces the segment to between the first and last points that are above the threshold
-
-    Args:
-        sgm (Segment): a segment
-        threshold (float): probability threshold
-
-    Returns:
-        Segment: new reduced segment
-    """
-    included_indices = np.where(sgm.probs >= threshold)[0]
-    
-    # return empty segment
-    if not len(included_indices):
-        return Segment(sgm.start, sgm.start, np.empty([0]))
-
-    i = included_indices[0]
-    j = included_indices[-1] + 1
-
-    sgm = Segment(sgm.start + i, sgm.start + j, sgm.probs[i:j])
-
-    return sgm
-
-
-def split_and_trim(
-    sgm: Segment, split_idx: int, threshold: float
-) -> tuple[Segment, Segment]:
-    """splits the input segment at the split_idx and then trims and returns the two resulting segments
-
-    Args:
-        sgm (Segment): input segment
-        split_idx (int): index to split the input segment
-        threshold (float): probability threshold
-
-    Returns:
-        tuple[Segment, Segment]: the two resulting segments
-    """
-
-    probs_a = sgm.probs[:split_idx]
-    sgm_a = Segment(sgm.start, sgm.start + len(probs_a), probs_a)
-
-    probs_b = sgm.probs[split_idx + 1 :]
-    sgm_b = Segment(sgm_a.end + 1, sgm.end, probs_b)
-
-    sgm_a = trim(sgm_a, threshold)
-    sgm_b = trim(sgm_b, threshold)
-
-    return sgm_a, sgm_b
-
-
-def update_yaml_content(
-    yaml_content: list[dict], segments: list[Segment], wav_name: str
-) -> list[dict]:
-    """extends the yaml content with the segmentation of this wav file
-
-    Args:
-        yaml_content (list[dict]): segmentation in yaml format
-        segments (list[Segment]): resulting segmentation from pdac
-        wav_name (str): name of the wav file
-
-    Returns:
-        list[dict]: extended segmentation in yaml format
-    """
-    for sgm in segments:
-        yaml_content.append(
-            {
-                "duration": sgm.duration,
-                "offset": sgm.offset,
-                "rW": 0,
-                "uW": 0,
-                "speaker_id": "NA",
-                "wav": wav_name,
-            }
-        )
-    return yaml_content
+from segment import Segment, split_and_trim #, trim
 
 
 class OnlineSegmenter:
 
     def __init__(self, args):
         self.args = args
-
 
         device = (
             torch.device(f"cuda:0")
@@ -142,12 +39,8 @@ class OnlineSegmenter:
         self.sfc_model.load_state_dict(checkpoint["state_dict"])
         self.sfc_model.eval()
 
-        # sgm = Segment(0, len(probs), probs)
-        # sgm = trim(sgm, args.dac_threshold)
-
         self.threshold = args.dac_threshold
         self.start = 0
-        # self.start = sgm.start
         self.leftover = Segment(self.start, self.start, np.empty(0))
 
         self.min_segm_len = int(TARGET_SAMPLE_RATE * args.dac_min_segment_length)
@@ -174,12 +67,7 @@ class OnlineSegmenter:
                 and (optionally) their ground truth values
         """
 
-        # duration_outframes = dataloader.dataset.duration_outframes
-
-#        talk_probs = np.empty(duration_outframes)
-#        talk_probs[:] = np.nan
         talk_probs = []
-        talk_targets = np.zeros(duration_outframes)
 
         for audio, targets, in_mask, out_mask, included, starts, ends in iter(data):
 
@@ -216,8 +104,6 @@ class OnlineSegmenter:
 #                if included[i] and end > start:
 #                    duration = end - start
 #                    talk_probs[start:end] = probs[i, :duration]
-#                    if targets is not None:
-#                        talk_targets[start:end] = targets[i, :duration].numpy()
 #                elif not included[i]:
 #                    talk_probs[start:end] = 0
 #
@@ -229,7 +115,7 @@ class OnlineSegmenter:
 #                talk_probs[max(0, j - 2) : min(duration_outframes, j + 3)]
 #            )
 
-        return talk_probs, talk_targets
+        return talk_probs
 
     def pstrm(
         self,
@@ -265,12 +151,6 @@ class OnlineSegmenter:
         current_sgm = _concat(self.leftover, new_sgm)
 
         first_part, second_part = _split(current_sgm, self.min_segm_len)
-#        print(f'{self.start=}, {end=}')
-#        print(f'{second_part.start=}, {second_part.end=}')
-#        print(f'{self.leftover.start=}, {self.leftover.end=}')
-#        print(f'{new_sgm.start=}, {new_sgm.end=}')
-#        print(f'{current_sgm.duration=}, {current_sgm.start=}, {current_sgm.end=}')
-#        print()
 
         if len(second_part.probs):
             sorted_indices = np.argsort(second_part.probs)
@@ -302,86 +182,19 @@ class OnlineSegmenter:
             wav_path, self.args.inference_segment_length, self.args.inference_times
         )
 
-        sgm_frame_probs = None
         for inference_iteration in range(self.args.inference_times):
-
             # create a dataloader for this fixed-length segmentation of the wav file
             dataset.fixed_length_segmentation(inference_iteration)
-#            dataloader = DataLoader(
-#                dataset,
-#                batch_size=self.args.inference_batch_size,
-#                num_workers=min(cpu_count() // 2, 4),
-#                shuffle=False,
-#                drop_last=False,
-#                collate_fn=segm_collate_fn,
-#            )
-#
-#        sgm_frame_probs /= self.args.inference_times
-#
-#        for s in self.process_audio(dataset):
-#            yield s
-        yield (s for s in self.process_audio(dataset))
+        # yield (s for s in self.process_audio(dataset))
+        for s in self.process_audio(dataset):
+            yield s
 
     def process_audio(self, dataset):
-        class _DataLoader:
-            def __init__(self, data, duration_outframes):
-                class _Dataset:
-                    duration_outframes = 0
-                self.dataset = _Dataset()
-                self.dataset.duration_outframes = duration_outframes
-                self.data = data
-
-            def __iter__(self):
-                # return iter(self.data)
-                return (segm_collate_fn([x]) for x in self.data)
-
-#        dataloader = _DataLoader(dataset, dataset.duration_outframes)
-#
-#        # get frame segmentation frame probabilities in the output space
-#        probs, _ = infer(
-#            self.wav2vec_model,
-#            self.sfc_model,
-#            dataloader,
-#            self.device,
-#        )
-#        if sgm_frame_probs is None:
-#            sgm_frame_probs = probs.copy()
-#        else:
-#            sgm_frame_probs += probs
-
-        def _split(sgm: Segment, split_idx: int) -> tuple[Segment, Segment]:
-            probs_a = sgm.probs[:split_idx]
-            sgm_a = Segment(sgm.start, sgm.start + len(probs_a), probs_a)
-            probs_b = sgm.probs[split_idx + 1:]
-            sgm_b = Segment(sgm_a.end + 1, sgm.end, probs_b)
-            return sgm_a, sgm_b
-
-#        all_probs = []
-#        for x in dataset:
-#            # get frame segmentation frame probabilities in the output space
-#            probs, _ = self.infer(
-#                self.wav2vec_model,
-#                self.sfc_model,
-#                [segm_collate_fn([x])],
-#                dataset.duration_outframes,
-#                self.device,
-#            )
-#            all_probs += probs
-#        probs = np.concatenate(all_probs, 1).squeeze()
-#
-#        sgm = Segment(0, len(probs), probs)
-#        while self.start < len(probs):
-#            end = min(self.leftover.start + self.max_segm_len, len(probs))
-#            end_sgm, _ = _split(sgm, end - sgm.start)
-#            _, new_sgm = _split(end_sgm, self.start - sgm.start)
-#            out_sgm = self.pstrm(new_sgm)
-#            if out_sgm is not None:
-#                yield out_sgm
-
         all_probs = np.empty(0)
+
         for x in dataset:
             # get frame segmentation frame probabilities in the output space
-            (probs,), _ = self.infer(
+            probs, = self.infer(
                 self.wav2vec_model,
                 self.sfc_model,
                 [segm_collate_fn([x])],
@@ -390,10 +203,8 @@ class OnlineSegmenter:
             )
             all_probs = np.concatenate((all_probs, probs.squeeze()), 0)
 
-            sgm = Segment(0, len(all_probs), all_probs)
             while (end := self.leftover.start + self.max_segm_len) < len(all_probs):
-                end_sgm, _ = _split(sgm, end - sgm.start)
-                _, new_sgm = _split(end_sgm, self.start - sgm.start)
+                new_sgm = Segment(self.start, end, all_probs[self.start:end])
                 out_sgm = self.pstrm(new_sgm)
                 if out_sgm is not None:
                     yield out_sgm
@@ -402,13 +213,6 @@ class OnlineSegmenter:
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--path_to_segmentation_yaml",
-        "-yaml",
-        type=str,
-        required=True,
-        help="absolute path to the yaml file to save the generated segmentation",
-    )
     parser.add_argument(
         "--path_to_checkpoint",
         "-ckpt",
@@ -476,29 +280,13 @@ if __name__ == "__main__":
         "If this argument is used, respecting the classification threshold conditions (p > thr)"
         "is more important than the length conditions (len < max)."
     )
-    parser.add_argument(
-        "--segmentation_method",
-        type=str,
-        default='pdac',
-        choices=['pdac', 'pstrm'],
-        help="Segmentation method to be used."
-    )
     args = parser.parse_args()
 
     segmenter = OnlineSegmenter(args)
 
-    yaml_content = []
-    for wav_path in tqdm(sorted(list(Path(args.path_to_wavs).glob("*.wav")))):
+    for wav_path in sorted(list(Path(args.path_to_wavs).glob("*.wav"))):
+        print(wav_path)
         for sgm in segmenter.segment(wav_path):
-            yaml_content = update_yaml_content(yaml_content, sgm, wav_path.name)
-
-    path_to_segmentation_yaml = Path(args.path_to_segmentation_yaml)
-    path_to_segmentation_yaml.parent.mkdir(parents=True, exist_ok=True)
-    with open(path_to_segmentation_yaml, "w") as f:
-        yaml.dump(yaml_content, f, default_flow_style=True)
-
-    print(
-        f"Saved SHAS segmentation with max={args.dac_max_segment_length} & "
-        f"min={args.dac_min_segment_length} at {path_to_segmentation_yaml}"
-    )
-
+            # yaml_content = update_yaml_content(yaml_content, sgm, wav_path.name)
+            print(f'{sgm.start / TARGET_SAMPLE_RATE:.2f} {sgm.end / TARGET_SAMPLE_RATE:.2f}')
+            #print(f'{sgm.duration:.4f} {sgm.start / TARGET_SAMPLE_RATE:.4f}')
